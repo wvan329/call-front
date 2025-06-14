10b353945eba7d6d74f5fe2bdc91608f83a16840
wgk <2287334379@qq.com>
2025-06-14 23:42:42 +0800

    yes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <nav>
-      <router-link to="/talk" class="text-blue-600 hover:underline">🗨️ 聊天</router-link>
-      <router-link to="/file" class="text-green-600 hover:underline">📁 文件传输</router-link>
+      <router-link to="/call/talk" class="text-blue-600 hover:underline">🗨️ 聊天</router-link>
+      <router-link to="/call/file" class="text-green-600 hover:underline">📁 文件传输</router-link>
     </nav>
 
     <router-view />
